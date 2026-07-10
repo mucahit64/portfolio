15c14b89ffae80d325a28ba7350b29574bccebfa
mucahit64 <mucahit.yaman@hotmail.com>
2026-07-10 19:29:41 +0300

    Add dark mode support to App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,16 @@ import Skills from '@/components/Skills.vue'
 import Projects from '@/components/Projects.vue'
 import Contact from '@/components/Contact.vue'
 import Footer from '@/components/Footer.vue'
+import { useDark } from '@/composables/useDark'
+
+const { isDark } = useDark()
 </script>
 
 <template>
-  <div class="fixed inset-0 w-full h-full overflow-y-auto screen-scroller select-none">
+  <div 
+    class="fixed inset-0 w-full h-full overflow-y-auto screen-scroller select-none transition-colors duration-300"
+    :class="isDark ? 'bg-[#212121] text-white' : 'bg-white text-gray-900'"
+  >
     <Topbar />
     <Home />
     <About />
